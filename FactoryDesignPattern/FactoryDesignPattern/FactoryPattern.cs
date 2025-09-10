9cb93b24a911b681783a5f013125f632bb67bfc1
supriya.shahi <supriya.shahi@DEL1-LHP-N77869>
2025-09-10 10:44:46 +0530

    Factory

---
+++ FactoryDesignPattern/FactoryDesignPattern/FactoryPattern.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public interface IPizza // Product
+    {
+        string Eat();
+    }
+
+    class VegPizza : IPizza
+    {
+        public string Eat()
+        {
+            return "Eating Veg Pizza";
+        }
+    }
+
+    class NonVegPizza : IPizza
+    {
+        public string Eat()
+        {
+            return "Eating Non-Veg Pizza";
+        }
+    }
+
+    interface IPizzaChef // Factory
+    {
+        IPizza PreparePizza();
+    }
+
+    class VegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new VegPizza();
+        }
+    }
+
+    class NonVegPizzaChef : IPizzaChef
+    {
+        public IPizza PreparePizza()
+        {
+            return new NonVegPizza();
+        }
+    }
+
+    class Waiter // Client
+    {
+        public IPizza GetPizza(string type)
+        {
+            IPizzaChef chef;
+            switch (type)
+            {
+                case "Veg":
+                    chef = new VegPizzaChef();
+                    break;
+                case "NonVeg":
+                    chef = new NonVegPizzaChef();
+                    break;
+                default:
+                    throw new Exception("Invalid Pizza Type");
+            }
+            return chef.PreparePizza();
+        }
+    }
+}
---
+++ FactoryDesignPattern/FactoryDesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using FactoryDesignPattern;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Waiter waiter = new Waiter(); // Client
+        IPizza pizza = waiter.GetPizza("Veg"); // Factory Method
+        Console.WriteLine(pizza.Eat()); // Product Method
+        pizza = waiter.GetPizza("NonVeg");
+        Console.WriteLine(pizza.Eat());
+        Console.ReadLine();
+    }
+}
