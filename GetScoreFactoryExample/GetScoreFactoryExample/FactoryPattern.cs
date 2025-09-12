d3e2f5a0dd1df5553f806432cde7e332b54c8c53
supriya.shahi <supriya.shahi@DEL1-LHP-N77869>
2025-09-12 12:08:13 +0530

    GetScoreFactory

---
+++ GetScoreFactoryExample/GetScoreFactoryExample/FactoryPattern.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetScoreFactoryExample
+{
+    public interface INews
+    {
+        string Get();
+    }
+
+    public class DDSports : INews
+    {
+        public string Get()
+        {
+            return "Score from DDSports";
+        }
+    }
+
+    public class StarSports : INews
+    {
+        public string Get()
+        {
+            return "Score from StarSports";
+        }
+    }
+
+    public interface INewsFactory
+    {
+        INews CreateNews();
+    }
+
+    public class DDSportsFactory : INewsFactory
+    {
+        public INews CreateNews()
+        {
+            return new DDSports();
+        }
+    }
+
+    public class StarSportsFactory : INewsFactory
+    {
+        public INews CreateNews()
+        {
+            return new StarSports();
+        }
+    }
+
+    class NewsFactory
+    {
+        private INewsFactory news;
+
+        public INews GetScore(string type) 
+        {
+            switch (type)
+            {
+                case "DDSports":
+                    news = new DDSportsFactory();
+                    break;
+                case "StarSports":      
+                    news = new StarSportsFactory();
+                    break;
+                default:
+                throw new Exception("Invalid news type");
+            }; 
+            return news.CreateNews();
+        }  
+    }
+}
---
+++ GetScoreFactoryExample/GetScoreFactoryExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using GetScoreFactoryExample;
+
+class Program
+{
+    public static void Main()
+    {
+       NewsFactory newsFactory = new NewsFactory();
+       INews news = newsFactory.GetScore("DDSports");
+       System.Console.WriteLine(news.Get());
+    }
+}
